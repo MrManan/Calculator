5adf1e2034013725bc4a734d3649b4b48a34b388
MrManan <mrmanan5657@gmail.com>
2021-06-13 21:11:49 +0500

    javascript-added

--- calculator.js
+++ calculator.js
@@ -1,7 +1,32 @@
-var btn = document.getElementById('get-result');
+var btn = document.getElementById('add');
 btn.onclick = function() {
-    var firstvalue = document.getElementById('first-name');
-    var secondvalue = document.getElementByI('second-value');
-    var yourresult = document.getElementById('your-result');
-
+    var firstvalue = document.getElementById('first-value');
+    var secondvalue = document.getElementById('second-value');
+    var result = document.getElementById('result');
+    var r = parseInt(firstvalue.value) + parseInt(secondvalue.value);
+    result.value = r;
+}
+var btn = document.getElementById('sub');
+btn.onclick = function() {
+    var firstvalue = document.getElementById('first-value');
+    var secondvalue = document.getElementById('second-value');
+    var result = document.getElementById('result');
+    var r = parseInt(firstvalue.value) - parseInt(secondvalue.value);
+    result.value = r;
+}
+var btn = document.getElementById('multiply');
+btn.onclick = function() {
+    var firstvalue = document.getElementById('first-value');
+    var secondvalue = document.getElementById('second-value');
+    var result = document.getElementById('result');
+    var r = parseInt(firstvalue.value) * parseInt(secondvalue.value);
+    result.value = r;
+}
+var btn = document.getElementById('divide');
+btn.onclick = function() {
+    var firstvalue = document.getElementById('first-value');
+    var secondvalue = document.getElementById('second-value');
+    var result = document.getElementById('result');
+    var r = parseFloat(firstvalue.value) / parseFloat(secondvalue.value);
+    result.value = r;
 }
